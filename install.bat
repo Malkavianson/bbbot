62b6cff18087c821e13d24db7029ee0dfb71ec7e
Malkavianson <alexneto.best@hotmail.com>
2020-04-25 14:59:31 -0300

    Fix install

--- install.bat
+++ install.bat
@@ -2,6 +2,6 @@ echo "Processo de instalacao."
 call npm install -f
 call npm update -f
 python -m pip install --upgrade pip
-call pip install -r requirements.txt
+call pip install -r requirements.txt --force --no-cache-dir
 
 pause
